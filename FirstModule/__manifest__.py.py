175ca15ba2d3b14b3cc75e07cc8720dd90937fdb
Isaac Espinosa <isaac.133.espinosa1@gmail.com>
2021-11-02 00:00:18 +0000

    Adding first module

---
+++ FirstModule/__manifest__.py.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'First Module',
+    
+    'summary': """FirstModule""",
+    
+    'description': """First Module
+    Test1
+    Test2
+    """,
+    
+    'Autor': 'Isaac',
+    
+    'website': 'https:www.WEB.com',
+    
+    'depends': ['base'],
+}
